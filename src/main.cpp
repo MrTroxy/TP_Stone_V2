51da21eaa60f7400a847c664f3a144df24b153be
David Tremblay <2036019@cegeprdl.ca>
2022-05-12 16:06:43 -0500

    Version 2.0

--- src/main.cpp
+++ src/main.cpp
@@ -49,47 +49,83 @@ MyButton *myButtonT5 = new MyButton();
 MyStone *myStone;
 
 // Fonction qui permet d'envoyer un int en hexadécimal
-string intToHexa(int value){
+string intToHexa(int value) {
   char buffer[10];
   sprintf(buffer , "0x%4X", value);
   return (buffer);
 };
 
 // Fonction qui permet d'envoyer un char en string
-string charToString(char str, string formatStr){
+string charToString(char str, string formatStr) {
   char buffer[10];
   sprintf(buffer , formatStr.c_str(), str);
   return (buffer);
 };
 
 // Fonction qui permet d'envoyer un int en string
-string intToString(int value, string formatStr){
+string intToString(int value, string formatStr) {
   char buffer[10];
   sprintf(buffer , formatStr.c_str(), value);
   return (buffer);
 };
 
-string nomBois = "Erable";
-string typeBois = "Dur";
-string origineBois = "Canada";
-string tempsDeSechage = "20 secondes";
-string temperatureMin = "25 Celcius";
+// Variables des informations sur le bois pour l'affichage
+char nomBoisAffichage[20] = "Bois : Erable";
+char typeBoisAffichage[20] = "Type : Dur";
+char origineBoisAffichage[20] = "Origine : Canada";
+char tempsDeSechageAffichage[40] = "Temps de séchage : 20 Secondes";
+char temperatureMinAffichage[40] = "Température Min : 25 Celcius";
+
+// Variable qui contient le compteur pour le temps de séchage
+int compteurTemps = 0;
+// Variable qui contient le compteur en string 
+char strCompteurTemps[64];
+// Variable qui contient le temps de sechage
+int tempsSechage = 20;
+// Variable qui contient la temperature minimum
+float temperatureMinimum = 25;
+// Variable qui contient la température obtenue en direct
+float temperatureEnDirect = 0;
+// Variable qui sert de bool pour dire si le four est en marche ou pas
+int cuissonDuFourEnCours = 0;
+
 
 //Thread qui permet de LOOPER et lire le contenu du port serie
 //avec l'aide du la fonction getValidsDatasIfExists
 void readStoneData() {
   datasRead rd = myStone->getValidsDatasIfExists();
-  //cout << "GData : " << intToHexa(abs(rd.id)) << " " << rd.command << " " << rd.name << " " << rd.type << "\n";
-  switch(rd.id){
 
-      case 0x0002: { //Version
-          cout << "GData : " << intToHexa(abs(rd.id)) << " " << rd.command << " " << rd.name << " " << rd.type << "\n";
-          string stoneVersion = rd.name;
-          cout << "Version : " <<  stoneVersion.c_str() << "\n";
+  myStone->setLabel("lbl_car_bois", nomBoisAffichage);
+  myStone->setLabel("lbl_car_type", typeBoisAffichage);
+  myStone->setLabel("lbl_car_origine", origineBoisAffichage);
+  myStone->setLabel("lbl_car_temps_sechage", tempsDeSechageAffichage);
+  myStone->setLabel("lbl_car_temp_min", temperatureMinAffichage);
 
-          break;
-          }
+  switch(rd.id) {
+    case 0x0002: { //Version
+
+      cout << "GData : " << intToHexa(abs(rd.id)) << " " << rd.command << " " << rd.name << " " << rd.type << "\n";
+      string stoneVersion = rd.name;
+      cout << "Version : " <<  stoneVersion.c_str() << "\n";
+
+    break;
+    }
+
+    case 0x1001: { // Bouton
+
+      string nomDuStone = rd.name;
+      string nomDuBouton = "btn_demarrage";
+            
+      if (nomDuStone == nomDuBouton && rd.type == 1) {
+        cout << "Démarrage du four!" << endl;
+
+        // On met le four en fonctionnement
+        cuissonDuFourEnCours = 1;
       }
+
+    break;
+    }
+  }
 
   if(rd.id<0) cout << "Data received ( id: : " << intToHexa(abs(rd.id)) << "  Command: " << rd.command << " Type: " << rd.type<< ")\n";
 }
@@ -102,9 +138,7 @@ void setup() {
   Serial.println("Stone serial Rxd is on pin: "+String(RXD2));
   myStone = new MyStone(115200, SERIAL_8N1, RXD2, TXD2);
 
-
   myTemp->init();
-
 
   myButtonT4->init(T4);
   myButtonT4->autoSensibilisation(); //Trouve la sensibilité automatiquement
@@ -112,37 +146,67 @@ void setup() {
   myButtonT5->init(T5);
   myButtonT5->autoSensibilisation(); //Trouve la sensibilité automatiquement
 
-  cout << string("Début du script")  << "\n";
+  cout << string("Démarrage du logiciel effectué")  << "\n";
 }
 
 void loop() {
-  float temperature = myTemp->getTemp();
+  temperatureEnDirect = myTemp->getTemp();
   char strTemperature[64];
-  sprintf(strTemperature, "%g Celcius", temperature);
-  delay(250);
-  myStone->setLabel("lbltemperature", strTemperature);
-  delay(250);
-
-  Serial.println("Voici la temperature detecte : " + String(myTemp->getTemp()));
+  sprintf(strTemperature, "%g Celcius", temperatureEnDirect);
+  myStone->setLabel("lbl_temperature", strTemperature);
 
   readStoneData();
 
+  // On vérifie si la cuisson du four doit commencer
+  if (cuissonDuFourEnCours == 1) {
+
+    myStone->setLabel("lbl_infos", "Température trop basse!");
+
+    while (temperatureEnDirect > temperatureMinimum) {
+      if(compteurTemps < tempsSechage) {
+        string stringTemps = "";
+        char strTempsSechage[64];
+
+        myStone->setLabel("lbl_infos", "Cuisson démarrée!");
+
+        sprintf(strTempsSechage, "%d s", tempsSechage);
+
+        compteurTemps++;
+        sprintf(strCompteurTemps, "%d /", compteurTemps);
+
+        stringTemps += strCompteurTemps;
+        stringTemps += strTempsSechage;
+                
+        const char* charTempsRestant = stringTemps.c_str();
+
+        myStone->setLabel("lbl_secondes_restantes", charTempsRestant);
+        delay(1000);
+      }
+      else {
+        myStone->setLabel("lbl_infos", "Le bois est maintenant seche!");
+        cout << "Cuisson du bois terminée!" << endl;
+
+        // Nous remettons les compteurs a 0 pour recommencer
+        compteurTemps = 0;
+        cuissonDuFourEnCours = 0;
+        myStone->setLabel("lbl_secondes_restantes", "0 / 20s");
+        break;
+      } 
+    }
+  }
+
   int buttonActionT4 = myButtonT4->checkMyButton();
 
-  if(buttonActionT4 > 2) 
-  {  //Si appuyé plus de 0.2 secondes
+  if(buttonActionT4 > 2) {  //Si appuyé plus de 0.2 secondes
     Serial.println("Button T4 pressed");
-  if(myStone) myStone->changePage();
-            }
-  int buttonActionT5 = myButtonT5->checkMyButton();
-      if(buttonActionT5 > 2)  {  //Si appuyé plus de 0.2 secondes
-            Serial.println("Button T5 pressed");
-            //Dans la version  1.2, nous allons remplacer ces lignes pour utiliser la
-            //méthode getVersion()
-            //char cmdFormat2[99];
-            //strcpy(cmdFormat2, "ST<{\"cmd_code\":\"sys_version\",\"type\":\"system\"}>ET");
-            //cout << cmdFormat2 << "\n";
-            //myStone->writeIt((char*)cmdFormat2);
-            if(myStone) myStone->getVersion();
-          }
+    if(myStone) myStone->changePage();
   }
+
+  int buttonActionT5 = myButtonT5->checkMyButton();
+
+  if(buttonActionT5 > 2)  {  //Si appuyé plus de 0.2 secondes
+    Serial.println("Button T5 pressed");
+
+    if(myStone) myStone->getVersion();
+  }
+}
